e96b62bf81fbb0bc228accfb7e6387f74e4218bf
danishshafiq030@gmail.com <shad@fmpdev.net>
2024-06-25 02:59:55 +0500

    remove the hard coded port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocket = require("ws");
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 // Initialize a WebSocket Server
 const wss = new WebSocket.Server({ noServer: true });
